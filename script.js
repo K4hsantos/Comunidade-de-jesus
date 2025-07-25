ce80f76329f0bd842d27f6e80d63bb1dde6efbc3
K4hsantos <kasantoshenrique@gmail.com>
2025-07-25 19:25:52 -0300

    adicionando funcionalidades com javascript

---
+++ script.js
@@ -0,0 +1,28 @@
+// js mobile
+
+const btn = document.querySelector("#copyBtn");
+
+function copiarPix() {
+    const chavePix = btn.dataset.pix
+    navigator.clipboard.writeText(chavePix)
+
+    .then(() => {
+        alert("Chave Pix copiada!");
+    })
+    .catch(() => {
+        alert("Erro ao copiar o Pix.");
+    });
+}
+
+btn.addEventListener("click", copiarPix);
+
+
+// js desktop
+
+const qrBtn = document.querySelector("#qrBtn");
+const qrContainer = document.querySelector("#qrContainer");
+
+qrBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  qrContainer.classList.toggle("show");
+});
